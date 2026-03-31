db97fa8a4b00ee0751650f0bf712fae4ebe67dbf
arnavupasanii <arnavupasani123@gmail.com>
2026-03-31 20:58:13 +0530

    script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+# Author: Arnav Upasani
 
-echo "Python Package Inspector :"		
-			
-if command -v python3 &> /dev/null		
-then
-    echo "Python is installed"			
-    python3 –version					
-    echo "Location: $(which python3)"  
+PACKAGE="git"
+
+echo "Checking package: $PACKAGE"
+echo "---------------------------"
+
+if command -v $PACKAGE >/dev/null 2>&1; then
+    echo "$PACKAGE is installed."
+    VERSION=$($PACKAGE --version)
+    echo "Version: $VERSION"
 else
-    echo "Python is NOT installed"			
-fi	
+    echo "$PACKAGE is NOT installed."
+fi
+
+# Case statement
+case $PACKAGE in
+    git) echo "Git: version control system enabling open collaboration" ;;
+    vlc) echo "VLC: open-source media player" ;;
+    firefox) echo "Firefox: open web browser" ;;
+    apache2) echo "Apache: powerful web server" ;;
+    *) echo "Unknown software" ;;
+esac
